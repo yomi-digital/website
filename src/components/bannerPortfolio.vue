dd9ae438204ae2f56e97c5b248b70c16f8ad7d70
Giuseppe-Napoli <giuseppe.napoli1993@hotmail.it>
2023-02-28 16:17:27 +0100

    banner project

--- src/components/bannerPortfolio.vue
+++ src/components/bannerPortfolio.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="banner cta-banner">
-    <a href="/#/portfolio">
-      <div :class="isMobile ? 'flex-column justify-content-center' : 'justify-content-between'"  class="d-flex align-items-center  h-100">
-        <div :class="{'mb-5' : !isMobile}" class="pb-5 text-uppercase"><p style="min-width:160px">{{ $t("banner.view") }}</p></div>
+    <a :href="'/#/portfolio?project=' + encodeURIComponent(selectedProject.name)">
+      <div
+        :class="
+          isMobile
+            ? 'flex-column justify-content-center'
+            : 'justify-content-between'
+        "
+        class="d-flex align-items-center h-100"
+      >
+        <div :class="{ 'mb-5': !isMobile }" class="pb-5 text-uppercase">
+          <p style="min-width: 160px">{{ $t("banner.view") }}</p>
+        </div>
         <div>
-          <h3 id="nameProject" class="text-uppercase cta-title mb-0">{{ $t("banner.project") }}</h3>
+          <h3 id="nameProject" class="text-uppercase cta-title mb-0">
+            {{ selectedProject.name }}
+          </h3>
         </div>
         <div v-if="!isMobile && !isTablet" class="pb-3">
-          <img src="../assets/images/arrow-right.svg" alt="">
+          <img src="../assets/images/arrow-right.svg" alt="" />
         </div>
       </div>
     </a>
@@ -21,11 +32,11 @@ export default {
   name: "bannerPortfolio",
   mixins: [checkViewport],
   data() {
-  return {
-    projects: projects,
-    selectedProjectIndex: 0, // Inizializza la proprietà selectedProjectIndex a 0
-  };
-},
+    return {
+      projects: projects,
+      selectedProject:{}
+    };
+  },
 
   mounted() {
     const app = this;
@@ -34,10 +45,10 @@ export default {
   methods: {
     randomProject() {
       let randomProject = Math.floor(Math.random() * projects.length);
+      this.selectedProject = projects[randomProject];
       setInterval(function () {
         randomProject = Math.floor(Math.random() * projects.length);
-        document.getElementById('nameProject').innerHTML = projects[randomProject].name
-        /* console.log(projects[randomProject].name); */
+        this.selectedProject = projects[randomProject];
       }, 5000);
     },
   },
