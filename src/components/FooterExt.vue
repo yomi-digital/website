c9009d0b82289298eb72d3f4b75b2c52b8d0a2e0
Livio Lombardo <livio@yomi.digital>
2024-03-13 09:51:49 +0100

    Update FooterExt - TGE LOGO

--- src/components/FooterExt.vue
+++ src/components/FooterExt.vue
@@ -52,6 +52,11 @@
               <img src="../assets/partners/Logo.png" alt="" />
             </a>
           </div>
+          <div class="slide">
+            <a href="https://theglasselite.com/" target="_blank">
+              <img src="../assets/partners/TGE_logo_bianco_tot.svg" alt="" />
+            </a>
+          </div>
           <template slot="prevButton"
             ><i class="fa-solid fa-chevron-left"></i
           ></template>
